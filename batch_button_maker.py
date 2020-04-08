a6ad85d6f9915349c9e7453e0caeac697bf9c395
Keith LeGrand <keithalegrand@gmail.com>
2020-04-08 11:30:02 -0400

    added conversion of SVG to PNG using inkscape
    Closes #2

--- batch_button_maker.py
+++ batch_button_maker.py
@@ -1,8 +1,8 @@
 import os
-
+from shlex import quote
 # define the name of the template file. Template file must be in the same folder as this .py file.
 my_template_file_name = "LibraryDIYTileTemplate.svg"
-
+inkscape_exec_mac = "/Applications/Inkscape.app/Contents/Resources/bin/inkscape"
 
 def duplicate_tile_with_new_text(template_file_name, text, output_dir):
     """ duplicate SVG file, replacing template text with new text
@@ -37,15 +37,19 @@ def duplicate_tile_with_new_text(template_file_name, text, output_dir):
 # process each text file in this direcory
 for filename in os.listdir('./'):
     if filename[-4:] == '.txt':
-        # check to see if a directory exists with the same name. If not, make one
+        # check to see if a dir exists with the same name. If not, make one.
         if not os.path.isdir(filename[:-4]):
             os.mkdir(filename[:-4])
 
         # process the lines in this text file
         fin = open(filename, "rt")
         for line in fin:
-            if line.rstrip() != "": # skip empty lines
-                duplicate_tile_with_new_text(my_template_file_name, line.rstrip(), filename[:-4])
+            if line.rstrip() != "":  # skip empty lines
+                svg_out_path = duplicate_tile_with_new_text(my_template_file_name, line.rstrip(), filename[:-4])
+                svg_out_path = os.path.abspath(svg_out_path)
+                # use inkscape to convert to svg
+                png_out_path = svg_out_path.replace(".svg", ".png")
+                os.system("{} -z -D {} -e {}".format(inkscape_exec_mac, quote(svg_out_path), quote(png_out_path)))
         print("Finished processing " + filename)
 
 print("done")
